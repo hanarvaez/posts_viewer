c7c4ca0637b6a2b773f9c4e3ce82ae139b92d242
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-02-02 01:00:47 -0500

    Added empty posts list activity

---
+++ app/src/main/java/co/com/monkeymobile/post_viewer/presentation/post_list/PostListActivity.kt
@@ -0,0 +1,12 @@
+package co.com.monkeymobile.post_viewer.presentation.post_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.com.monkeymobile.post_viewer.R
+
+class PostListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_list)
+    }
+}
--- app/src/main/java/co/com/monkeymobile/post_viewer/presentation/splash/SplashActivity.kt
+++ app/src/main/java/co/com/monkeymobile/post_viewer/presentation/splash/SplashActivity.kt
@@ -1,9 +1,11 @@
 package co.com.monkeymobile.post_viewer.presentation.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import co.com.monkeymobile.post_viewer.R
+import co.com.monkeymobile.post_viewer.presentation.post_list.PostListActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,11 @@ class SplashActivity : AppCompatActivity() {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startActivity(Intent(this, PostListActivity::class.java))
         finish()
     }
 }

5b788b847e4b5c82eb40757cf175dc3d43eea5d9
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-02-03 18:19:30 -0500

    Implement ui changes required programatically from user details fragment

--- app/src/main/java/co/com/monkeymobile/post_viewer/presentation/post_detail/user/UserDetailsFragment.kt
+++ app/src/main/java/co/com/monkeymobile/post_viewer/presentation/post_detail/user/UserDetailsFragment.kt
@@ -49,14 +49,88 @@ class UserDetailsFragment :
     }
 
     private fun buildLoadingState() {
+        with(binding) {
+            progress.visibility = View.VISIBLE
+            errorMessage.visibility = View.GONE
+
+            labelName.visibility = View.INVISIBLE
+            labelUserName.visibility = View.INVISIBLE
+            labelEmail.visibility = View.INVISIBLE
+            labelAddress.visibility = View.INVISIBLE
+            labelZipCode.visibility = View.INVISIBLE
+            labelPhone.visibility = View.INVISIBLE
+            labelWebsite.visibility = View.INVISIBLE
+            labelCompany.visibility = View.INVISIBLE
+
+            name.visibility = View.INVISIBLE
+            username.visibility = View.INVISIBLE
+            email.visibility = View.INVISIBLE
+            address.visibility = View.INVISIBLE
+            zipCode.visibility = View.INVISIBLE
+            phone.visibility = View.INVISIBLE
+            website.visibility = View.INVISIBLE
+            company.visibility = View.INVISIBLE
+        }
 
     }
 
     private fun buildContentState(state: UserDetailsViewState.Content) {
+        with(binding) {
+            labelName.visibility = View.VISIBLE
+            labelUserName.visibility = View.VISIBLE
+            labelEmail.visibility = View.VISIBLE
+            labelAddress.visibility = View.VISIBLE
+            labelZipCode.visibility = View.VISIBLE
+            labelPhone.visibility = View.VISIBLE
+            labelWebsite.visibility = View.VISIBLE
+            labelCompany.visibility = View.VISIBLE
 
+            name.visibility = View.VISIBLE
+            name.text = state.user.name
+            username.visibility = View.VISIBLE
+            username.text = state.user.username
+            email.visibility = View.VISIBLE
+            email.text = state.user.email
+            address.visibility = View.VISIBLE
+            address.text =
+                "${state.user.address.city}, ${state.user.address.street}, ${state.user.address.suite}"
+            zipCode.visibility = View.VISIBLE
+            zipCode.text = state.user.address.zipcode
+            phone.visibility = View.VISIBLE
+            phone.text = state.user.phone
+            website.visibility = View.VISIBLE
+            website.text = state.user.website
+            company.visibility = View.VISIBLE
+            company.text = state.user.company.name
+
+            progress.visibility = View.GONE
+            errorMessage.visibility = View.GONE
+        }
     }
 
     private fun buildErrorState() {
+        with(binding) {
+            progress.visibility = View.GONE
+            errorMessage.visibility = View.VISIBLE
+
+            labelName.visibility = View.INVISIBLE
+            labelUserName.visibility = View.INVISIBLE
+            labelEmail.visibility = View.INVISIBLE
+            labelAddress.visibility = View.INVISIBLE
+            labelZipCode.visibility = View.INVISIBLE
+            labelPhone.visibility = View.INVISIBLE
+            labelWebsite.visibility = View.INVISIBLE
+            labelCompany.visibility = View.INVISIBLE
+
+            name.visibility = View.INVISIBLE
+            username.visibility = View.INVISIBLE
+            email.visibility = View.INVISIBLE
+            address.visibility = View.INVISIBLE
+            zipCode.visibility = View.INVISIBLE
+            phone.visibility = View.INVISIBLE
+            website.visibility = View.INVISIBLE
+            company.visibility = View.INVISIBLE
+        }
 
     }
 }

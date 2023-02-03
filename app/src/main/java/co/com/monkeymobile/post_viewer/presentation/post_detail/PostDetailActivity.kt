4993adb1b71e1e8c22be05056ae984ec85f2126b
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-02-03 15:28:42 -0500

    Implemented function for add fragments into post detail activity

--- app/src/main/java/co/com/monkeymobile/post_viewer/presentation/post_detail/PostDetailActivity.kt
+++ app/src/main/java/co/com/monkeymobile/post_viewer/presentation/post_detail/PostDetailActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import co.com.monkeymobile.post_viewer.databinding.ActivityPostDetailBinding
+import co.com.monkeymobile.post_viewer.presentation.post_detail.post.PostDetailFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,9 +16,10 @@ class PostDetailActivity : AppCompatActivity() {
 
         const val EXTRA_POST_ID = "postId"
 
-        fun getIntent(context: Context, postId: Int) = Intent(context, PostDetailActivity::class.java).apply {
-            putExtra(EXTRA_POST_ID, postId)
-        }
+        fun getIntent(context: Context, postId: Int) =
+            Intent(context, PostDetailActivity::class.java).apply {
+                putExtra(EXTRA_POST_ID, postId)
+            }
     }
 
     private lateinit var binding: ActivityPostDetailBinding
@@ -26,5 +29,15 @@ class PostDetailActivity : AppCompatActivity() {
         binding = ActivityPostDetailBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val postId = intent.getIntExtra(EXTRA_POST_ID, 0)
+        addFragment(PostDetailFragment.newInstance(postId))
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(binding.fragmentContainer.id, fragment)
+            .commit()
     }
 }

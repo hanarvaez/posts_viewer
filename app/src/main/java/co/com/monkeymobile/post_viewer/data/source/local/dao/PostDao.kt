bc7c0f7f4759f23b1a150ff36440dbae20b00075
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-02-07 16:05:21 -0500

    Add attributes and logic for mark a post as favorite, also for return first favorite posts

--- app/src/main/java/co/com/monkeymobile/post_viewer/data/source/local/dao/PostDao.kt
+++ app/src/main/java/co/com/monkeymobile/post_viewer/data/source/local/dao/PostDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import co.com.monkeymobile.post_viewer.data.source.local.entities.POST_ID_COLUMN_NAME
+import co.com.monkeymobile.post_viewer.data.source.local.entities.POST_IS_FAVORITE_COLUMN_NAME
 import co.com.monkeymobile.post_viewer.data.source.local.entities.POST_TABLE_NAME
 import co.com.monkeymobile.post_viewer.data.source.local.entities.PostEntity
 
@@ -14,7 +15,7 @@ interface PostDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertPost(vararg postEntity: PostEntity)
 
-    @Query("SELECT * FROM $POST_TABLE_NAME")
+    @Query("SELECT * FROM $POST_TABLE_NAME ORDER BY $POST_IS_FAVORITE_COLUMN_NAME DESC, $POST_ID_COLUMN_NAME ASC")
     fun getAllPosts(): List<PostEntity>
 
     @Query("SELECT * FROM $POST_TABLE_NAME WHERE $POST_ID_COLUMN_NAME = :postId")

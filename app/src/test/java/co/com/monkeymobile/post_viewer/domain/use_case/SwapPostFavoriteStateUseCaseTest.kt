6685f148886004764f04de88a74f4cbf4e5e0d98
Hector A. Narvaez <hanarvaez92@gmail.com>
2023-02-13 20:21:06 -0500

    Add tests for SwapPostFavoriteStateUseCase

---
+++ app/src/test/java/co/com/monkeymobile/post_viewer/domain/use_case/SwapPostFavoriteStateUseCaseTest.kt
@@ -0,0 +1,54 @@
+package co.com.monkeymobile.post_viewer.domain.use_case
+
+import android.util.Log
+import co.com.monkeymobile.post_viewer.domain.repository.PostRepository
+import io.mockk.every
+import io.mockk.mockkStatic
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+class SwapPostFavoriteStateUseCaseTest {
+
+    private val postRepository: PostRepository = mock()
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default
+
+    private val swapPostFavoriteStateUseCase =
+        SwapPostFavoriteStateUseCase(postRepository, dispatcher)
+
+    private val params: SwapPostFavoriteStateUseCaseParams = mock()
+
+    @Before
+    fun prepareEnvironment() {
+        mockkStatic(Log::class)
+        every { Log.v(any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+        every { Log.i(any(), any()) } returns 0
+        every { Log.e(any(), any()) } returns 0
+    }
+
+    @Test
+    fun `test execute when success`() = runTest {
+        whenever(params.postId).thenReturn(0)
+
+        val result = swapPostFavoriteStateUseCase(params)
+
+        assert(result is Result.Success)
+    }
+
+    @Test
+    fun `test execute when exception`() = runTest {
+        whenever(params.postId).thenReturn(0)
+        whenever(postRepository.swapPostfavoriteState(params.postId)).thenThrow(RuntimeException())
+
+        val result = swapPostFavoriteStateUseCase(params)
+
+        assert(result is Result.Error)
+    }
+}
